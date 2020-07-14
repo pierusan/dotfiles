97712c036b5cfba7c0820507d65a7f5af9974a25
Pierre Amelot <pierre.p.amelot@gmail.com>
2020-07-13 23:17:49 -0700

    Add chocolatey dummy script

---
+++ chocolatey.ps1
@@ -0,0 +1,7 @@
+# Install chocolatey
+# https://chocolatey.org/docs/installation
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Stuff
+# https://chocolatey.org/docs/commands-reference
+# TODO: Install Blender, Chrome, VScode, Windows Terminal, VLC, MalwareBytes?, Skype, Zoom, Google File Stream, Wireshark?, youtube-dl, Unity
